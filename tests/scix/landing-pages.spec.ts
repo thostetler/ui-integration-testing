d7bc195a68219ee245c0400634e0df7db780c695
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-04-02 13:09:05 -0400

    get landing page tests working

--- tests/scix/landing-pages.spec.ts
+++ tests/scix/landing-pages.spec.ts
@@ -16,26 +16,12 @@ test('Classic form loads properly', { tag: ['@smoke'] }, async ({ page }, testIn
 
   await test.step('Confirm the page loaded correctly', async () => {
     await expect(page.locator('form')).toMatchAriaSnapshot({ name: `${name}.aria.yml` });
-
-    // author
     await expect(page.getByRole('textbox', { name: 'Author' })).toBeEmpty();
-
-    // object
     await expect(page.getByRole('textbox', { name: 'Object' })).toBeEmpty();
-
-    // pubdate
     await expect(page.getByRole('textbox', { name: 'Publication Date End' })).toBeEmpty();
-
-    // title
     await expect(page.getByRole('textbox', { name: 'Title' })).toBeEmpty();
-
-    // abstract/keywords
     await expect(page.getByRole('textbox', { name: 'Abstract / Keywords' })).toBeEmpty();
-
-    // publications
     await expect(page.getByRole('combobox', { name: 'bibstem picker' })).toBeEmpty();
-
-    // sort
     await expect(page.getByRole('combobox', { name: 'sort' })).toBeEmpty();
   });
 
@@ -43,33 +29,32 @@ test('Classic form loads properly', { tag: ['@smoke'] }, async ({ page }, testIn
   await test.step('Check for a11y violations', async () => await a11yCheck(page, name, testInfo));
 });
 
-test.fixme('Modern form loads properly', { tag: ['@smoke'] }, async ({ page }, testInfo) => {
+test('Modern form loads properly', { tag: ['@smoke'] }, async ({ page }, testInfo) => {
   const name = 'modern-form';
   await page.goto(ROUTES.ROOT);
   await expect(page).toHaveURL(new RegExp(`${ROUTES.ROOT}`));
 
   await test.step('Confirm the page loaded correctly', async () => {
-    await expect(page.getByLabel('Start typing a query here to')).toBeEmpty();
+    await expect(page.locator('#main-content')).toMatchAriaSnapshot({ name: `${name}.aria.yml` });
+    await expect(page.getByTestId('search-input')).toBeEmpty();
   });
 
   await test.step('Checking for visual regressions', async () => await visualCheck(page, name));
   await test.step('Check for a11y violations', async () => await a11yCheck(page, name, testInfo));
 });
 
-test.fixme('Paper form loads properly', { tag: ['@smoke'] }, async ({ page }, testInfo) => {
+test('Paper form loads properly', { tag: ['@smoke'] }, async ({ page }, testInfo) => {
   const name = 'paper-form';
   await page.goto(ROUTES.PAPER_FORM);
   await expect(page).toHaveURL(new RegExp(`${ROUTES.PAPER_FORM}`));
 
   await test.step('Confirm the page loaded correctly', async () => {
-    await expect(page.locator('#landing-page-layout')).toContainText('Journal Search');
-    await expect(page.locator('#landing-page-layout')).toContainText('Reference Query');
-    await expect(page.locator('#landing-page-layout')).toContainText('Bibliographic Code Query');
-    await expect(page.getByLabel('Publication')).toBeEmpty();
+    await expect(page.locator('#main-content')).toMatchAriaSnapshot({ name: `${name}.aria.yml` });
+    await expect(page.getByRole('combobox', { name: 'bibstem picker' })).toBeEmpty();
     await expect(page.getByLabel('Year')).toBeEmpty();
     await expect(page.getByLabel('Volume')).toBeEmpty();
-    await expect(page.getByLabel('Page/ID')).toBeEmpty();
-    await expect(page.getByLabel('Reference')).toBeEmpty();
+    await expect(page.getByLabel('Page / Id')).toBeEmpty();
+    await expect(page.getByRole('textbox', { name: 'reference' })).toBeEmpty();
     await expect(page.getByLabel('List of Bibcodes')).toBeEmpty();
   });
 
